6cb62155a07ba022e4aede647f107a98e832aed8
NickWright <nwright729@yahoo.com>
2017-03-28 11:10:10 -0700

    Image update project-script

--- project-script.js
+++ project-script.js
@@ -7,44 +7,44 @@
             {
               title:"Pomodoro Clock",
               description:"A pomodoro clock designed to help you stay focused on work. Adjustable work session and break times in a convenient UI. ",
-              url:"http://codepen.io/nwright/pen/dpzvRm",
-              background:"http://res.cloudinary.com/nwright/image/upload/v1490629584/2017-03-27-08-27-codepen.io_bk8bv2.png",
-              position:'50%'
+              url:"#",
+              background:"http://res.cloudinary.com/nwright/image/upload/v1490723947/2017-03-28-10-52-codepen.io_z272ej.png",
+              position:'59% -12px'
             },
             {
               title:"Tic Tac Toe",
               description:'Try your hand at Tic Tac Toe against the Minimax algorithm. It hasn\'t been beaten yet. ',
-              url:"http://codepen.io/nwright/pen/RprZgE",
+              url:"#",
               background:"http://res.cloudinary.com/nwright/image/upload/v1490629542/2017-03-27-08-45-s.codepen.io_xofhqo.png",
               position:'46% 22%'
             },
             {
               title:'Simon',
               description:'Test your memory with a game of Simon. Can be set to strict mode which will set you back to the beginning if you make a mistake. ',
-              url:"http://codepen.io/nwright/pen/KNvmjO",
-              background:"http://res.cloudinary.com/nwright/image/upload/v1490629564/2017-03-27-08-31-codepen.io_nl2zcm.png",
-              position:'32% 36%'
+              url:"#",
+              background:"http://res.cloudinary.com/nwright/image/upload/v1490723956/2017-03-28-10-53-codepen.io_mnxiiw.png",
+              position:'29% 36%'
               
             },
             {
               title:'Wiki-Viewer',
               description:'Search wikipedia for interesting articles',
-              url:"http://codepen.io/nwright/pen/LkqaYN",
-              background:"http://res.cloudinary.com/nwright/image/upload/c_scale,h_300,w_300/v1489679587/2017-03-16-08-39-s.codepen.io_oqfxsc.png",
-              position:'0 20px'
+              url:"#",
+              background:"http://res.cloudinary.com/nwright/image/upload/v1490723977/2017-03-28-10-56-codepen.io_j0ybmu.png",
+              position:'37% 20px'
             },
             {
               title:'Javascript Calculator',
               description:'A functional online calculator made with Javascript and JQuery',
-              url:"http://codepen.io/nwright/pen/wzJyEX",
-              background:"http://res.cloudinary.com/nwright/image/upload/v1490629570/2017-03-27-08-30-codepen.io_zqdruo.png",
-              position:'52% 2%'
+              url:"#",
+              background:"http://res.cloudinary.com/nwright/image/upload/v1490723971/2017-03-28-10-55-codepen.io_ug5fh2.png",
+              position:'51% 2%'
             },
             {
               title:'Quote Generator',
               description:'Generates quotes using a free API. Find a quote you like and post it to Twitter',
-              url:"http://codepen.io/nwright/pen/WxYXGZ",
-              background:"http://res.cloudinary.com/nwright/image/upload/v1490629739/2017-03-27-08-48-codepen.io_mrgfka.png",
+              url:"#",
+              background:"http://res.cloudinary.com/nwright/image/upload/v1490723964/2017-03-28-10-54-codepen.io_numvjn.png",
               position:'35% 4%'
             }]
         }
